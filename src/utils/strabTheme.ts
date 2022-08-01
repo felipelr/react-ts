b6115af2bd7aebaec00cb47aa06db6b63f85099c
Felipe de Lima Rodrigues <felipe.lima.flr@gmail.com>
2022-08-01 10:04:51 -0300

    ajuste no tema

--- src/utils/strabTheme.ts
+++ src/utils/strabTheme.ts
@@ -1,4 +1,4 @@
-import { createTheme } from "@material-ui/core/styles";
+import { createTheme } from "@mui/material";
 import { strab_blue, strab_yellow, white } from "./colors";
 
 declare module "@mui/material/styles" {
@@ -6,13 +6,11 @@ declare module "@mui/material/styles" {
     neutral: Palette["primary"];
   }
 
-  // allow configuration using `createTheme`
   interface PaletteOptions {
     neutral?: PaletteOptions["primary"];
   }
 }
 
-// Update the Button's color prop options
 declare module "@mui/material/Button" {
   interface ButtonPropsColorOverrides {
     neutral: true;
